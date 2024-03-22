a1a00e6f192bbcc8a3d2da9b801da40a062f19ee
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-03-22 19:57:24 -0400

    add tf lesson

--- llm-scraper/tf/rds.tf
+++ llm-scraper/tf/rds.tf
@@ -1,10 +1,5 @@
-locals { 
-    db_config = {
-        db_name = "job_scraper"
-        instance_name = "scraper_db"
-        username = "postgres"
-        password = "password"
-    }
+provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_db_instance" "postgres_db" {
@@ -19,24 +14,5 @@ resource "aws_db_instance" "postgres_db" {
   parameter_group_name = "default.postgres16"
   skip_final_snapshot  = true
   publicly_accessible  = true
-  # remove this
-  vpc_security_group_ids = [aws_security_group.postgres_access.id]
-  tags = { Name = local.db_config.instance_name }
-}
-
-resource "aws_security_group" "postgres_access" {
-  name = "scraper postgres access"
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    # remove
-    security_groups = [aws_security_group.web_app.id]
-  }
-}
-
-
-output "rds_connection_instructions" {
-  value = "psql -d ${aws_db_instance.postgres_db.db_name} -h ${aws_db_instance.postgres_db.endpoint} -U ${aws_db_instance.postgres_db.username}"
+  
 }
